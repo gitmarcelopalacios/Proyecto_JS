99252c332021fac240fc03586e1a6672c4b41eb4
Marcelo Carlos Palacios <correodemarcelopalacios@gmail.com>
2024-10-23 15:45:35 -0300

    guardar

---
+++ archivo.js
@@ -0,0 +1,40 @@
+// var es inicializacion global
+
+var nombre ='Pedro';
+var Nombre ='Anny';
+var _nombre ='Tomás';
+var NOMBRE ='Juana';
+var $_nombre ='Antonio';
+
+// let es inicializacion local
+
+// let saludo = 'hola'
+
+// document.write(saludo)
+
+// operadores de asignacion, incremento y decremento
+
+let a=1
+document.write('Valor de a: ', a)
+document.write('<br>')
+a++ // aumenta el valor en 1
+document.write('Valor de a: ', a)
+document.write('<br>')
+a+=5 // aumento variable
+document.write('Valor de a: ', a)
+document.write('<br>')
+a-- // resta valor en 1
+document.write('Valor de a: ', a)
+document.write('<br>')
+a-=3 // decremento variable
+document.write('Valor de a: ', a)
+document.write('<br>')
+a*=2 // multiplica variable 
+document.write('Valor de a: ', a)
+document.write('<br>')
+a/=3 // divido variable 
+document.write('Valor de a: ', a)
+document.write('<br>')
+a**=3 // potencio variable 
+document.write('Valor de a: ', a)
+document.write('<br>')
